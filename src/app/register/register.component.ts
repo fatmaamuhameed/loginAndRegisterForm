aa45935dc35c468609fdb56f9a9c38e001c2638b
ahmedadel101 <59322068+ahmedadel101@users.noreply.github.com>
2021-12-06 10:17:31 +0200

    edit forms

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-register',
@@ -7,6 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  registerForm = new FormGroup({
+    name: new FormControl(null,[Validators.required,Validators.minLength(3),Validators.maxLength(20)]),
+    password_confirmation: new FormControl(null,[Validators.required,Validators.maxLength(15)]),
+    email: new FormControl(null,[Validators.required,Validators.email]),
+    password: new FormControl(null,[Validators.required,Validators.pattern("^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$"),Validators.maxLength(15)]),
+  })
   constructor() { }
 
   ngOnInit(): void {
