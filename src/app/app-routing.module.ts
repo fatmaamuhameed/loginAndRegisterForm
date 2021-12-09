61df18d389260bc401b75414b002d6d261f59323
ahmedadel101 <59322068+ahmedadel101@users.noreply.github.com>
2021-12-09 16:58:10 +0200

    add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,17 @@
-import { UploadImagesComponent } from './upload-images/upload-images.component';
 import { ProfileComponent } from './profile/profile.component';
 import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './register/register.component';
-import { UploadComponent } from './upload/upload.component';
 
 const routes: Routes = [
   {path:'' , component:AppComponent , children:[
     {path:'login' , component:LoginComponent},
     {path:'',component:RegisterComponent},
     {path:'register',component:RegisterComponent},
-    {path:'profile',component:ProfileComponent},
-    {path:'image' , component:UploadComponent},
+    {path:'profile',component:ProfileComponent}
 
-    {path:'uploadImages' , component: UploadImagesComponent}
   ] }
 ];
 
